737ca0154a5ffcf11b97b893ef7d625fdaead7d7
AnhDD <viet.dm@zinza.com.vn>
2022-05-18 15:06:49 +0700

    Move hook to file

--- src/store.tsx
+++ src/store.tsx
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import counterSlice from './StoreSlice/CounterSlice';
 
 const rootStore = configureStore({
@@ -11,5 +10,3 @@ const rootStore = configureStore({
 export const store = rootStore;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
