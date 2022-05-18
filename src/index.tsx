313d8f46088ff5fe643e9c51183957dee69d37f2
AnhDD <viet.dm@zinza.com.vn>
2022-05-18 14:50:55 +0700

    Add React Route

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store';
-
-const rootElement = document.getElementById('root');
+import ListRoutes from './Routes/Route';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>{ListRoutes()}</BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  rootElement
+  document.getElementById('root')
 );
 
 reportWebVitals();
