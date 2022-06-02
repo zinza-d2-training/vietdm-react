1f702927d3c2dd7462ec2518ed78ed14057067da
Minh Việt <105332067+viet-dm@users.noreply.github.com>
2022-06-02 11:24:36 +0700

    Implement Forgot Password #6 (#13)

--- src/Components/Auth/Auth.tsx
+++ src/Components/Auth/Auth.tsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Register from './Register';
 import Assets from '../../Assets';
 import Login from './Login';
+import ForgotPassword from './ForgotPassword';
 import styled from 'styled-components';
 
 const AuthBox = styled.div`
@@ -29,6 +30,7 @@ export default function Auth() {
         <Routes>
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BoxRight>
