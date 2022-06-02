1d290c1f5db116722c6ddc17ef0432404b7f442f
AnhDD <viet.dm@zinza.com.vn>
2022-06-02 11:21:37 +0700

    Fix with feedback

--- src/Components/Auth/ForgotPassword.tsx
+++ src/Components/Auth/ForgotPassword.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { TextField, Typography } from '@mui/material';
+import { Stack, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { PropAttributeInterface } from '../../Types';
 import { useNavigate } from 'react-router-dom';
 import { LoadingButton } from '@mui/lab';
 import * as yup from 'yup';
-import styled from 'styled-components';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { sleep } from '../../Common';
 
@@ -38,21 +37,16 @@ const schemaValidate = yup.object({
   email: yup.string().email(MgsValidate.email.pattern).required(MgsValidate.email.required)
 });
 
-const FormForgot = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  flex-direction: column;
-`;
-
 function ForgotPassword() {
   const {
     handleSubmit,
     control,
     formState: { isValid, isSubmitted }
   } = useForm<FormValues>({
-    resolver: yupResolver(schemaValidate)
+    resolver: yupResolver(schemaValidate),
+    defaultValues: {
+      email: ''
+    }
   });
 
   const navigate = useNavigate();
@@ -63,32 +57,14 @@ function ForgotPassword() {
     await sleep(2000);
     navigate('/auth/login');
   };
-
   return (
-    <FormForgot>
-      <Box
-        sx={{
-          overflow: 'hidden auto',
-          width: '100%',
-          padding: '20px 0',
-          maxHeight: '100vh'
-        }}>
-        <Box
-          sx={{
-            width: '75%',
-            maxWidth: 450,
-            margin: 'auto'
-          }}>
+    <Stack justifyContent="center" alignItems="center" height="100%">
+      <Box width="100%" padding="20px 0" maxHeight="100vh" overflow="hidden auto">
+        <Box width="75%" maxWidth={450} m="auto">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <Typography
-              variant="h6"
-              component="p"
-              sx={{
-                textAlign: 'center',
-                fontSize: '18px'
-              }}>
+            <Typography variant="h6" component="p" textAlign="center" fontSize={18}>
               Để khôi phục mật khẩu, vui lòng nhập đúng email bạn đã dùng để đăng ký
-              <Box component="span" sx={{ color: 'red', ml: 1 }}>
+              <Box component="span" color="red" ml={1}>
                 (*)
               </Box>
             </Typography>
@@ -106,17 +82,12 @@ function ForgotPassword() {
                   label="Email (*)"
                   error={!!error}
                   helperText={error?.message}
-                  onChange={field.onChange}
+                  {...field}
                 />
               )}
             />
 
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                mt: 1
-              }}>
+            <Stack justifyContent="center" flexDirection="row" mt={1}>
               <LoadingButton
                 disabled={requesting}
                 loading={false}
@@ -140,11 +111,11 @@ function ForgotPassword() {
                 }}>
                 Gửi
               </LoadingButton>
-            </Box>
+            </Stack>
           </form>
         </Box>
       </Box>
-    </FormForgot>
+    </Stack>
   );
 }
 
